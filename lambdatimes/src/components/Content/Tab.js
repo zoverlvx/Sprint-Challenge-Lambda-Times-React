902ac52ab46d52b59fda32780bb4691997f4fd25
Zach Overhulser <zoverhulser@gmail.com>
2019-04-05 17:15:54 +0000

    Specifies types

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Tab = props => {
+	console.log("props for tab: ", props.tab)
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
@@ -15,5 +17,10 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+	tab: PropTypes.string,
+	selectedTab: PropTypes.string,
+	selectTabHandler: PropTypes.func	
+}
 
 export default Tab;
