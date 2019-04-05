27b9144289501dce0c4597b8da1def7d185209a0
Zach Overhulser <zoverhulser@gmail.com>
2019-04-05 18:21:30 +0000

    Trying to figure out filter

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -38,6 +38,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+	if(this.state.selected === "all") return this.state.cards;
     return this.state.cards;
   };
 
