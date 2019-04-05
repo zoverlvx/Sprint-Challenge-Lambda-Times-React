53c750498431b59c3677d132e7f8bcc501bf20b2
Zach Overhulser <zoverhulser@gmail.com>
2019-04-05 17:06:57 +0000

    Specifies types and handles alt for image.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Card = ({data}) => {
-
+	console.log(data)
   return (
     <div className="card">
       <div className="headline">{data.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={data.img} />
+          <img alt={data.tab} src={data.img} />
         </div>
         <span>By {data.author}</span>
       </div>
@@ -16,5 +17,13 @@ const Card = ({data}) => {
 };
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+	data: PropTypes.shape({
+		author: PropTypes.string,
+		headline: PropTypes.string,
+		img: PropTypes.string,
+		tab: PropTypes.string
+	})	
+}
 
 export default Card;
